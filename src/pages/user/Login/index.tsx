9c0aad5d525ae13eee0e1fdb4c088625530b1d3c
dengchaosheng <dengchaosheng@jinshuju.net>
2022-09-11 15:42:36 +0800

    Remove other login types

--- src/pages/user/Login/index.tsx
+++ src/pages/user/Login/index.tsx
@@ -94,16 +94,6 @@ const Login: React.FC = () => {
           initialValues={{
             autoLogin: true,
           }}
-          actions={[
-            <FormattedMessage
-              key="loginWith"
-              id="pages.login.loginWith"
-              defaultMessage="其他登录方式"
-            />,
-            <AlipayCircleOutlined key="AlipayCircleOutlined" className={styles.icon} />,
-            <TaobaoCircleOutlined key="TaobaoCircleOutlined" className={styles.icon} />,
-            <WeiboCircleOutlined key="WeiboCircleOutlined" className={styles.icon} />,
-          ]}
           onFinish={async (values) => {
             await handleSubmit(values as API.LoginParams);
           }}
